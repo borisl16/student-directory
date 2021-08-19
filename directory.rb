8bde8a2437ecf4f7e32a3e429b8e82a7e4cddeae
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-19 10:44:44 +0100

    List of students

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-student_count = 11
-
 students = [
     "Dr. Hannibal Lecter",
     "Darth Vader",
@@ -16,16 +14,7 @@ students = [
 
 puts "The students of Vallains Academy"
 puts "_______________"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
+students.each do |student|
+    puts student
 
-puts "Ovelall, we have #{student_count} great students"
+puts "Ovelall, we have #{students.count} great students"
