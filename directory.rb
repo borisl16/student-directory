1a44595046eb2779da9729d67e0bd7a0b96e5305
Borislav Lazarov <lazarov_borislav@yahoo.com>
2021-08-22 12:47:02 +0100

    interactive_menu

--- directory.rb
+++ directory.rb
@@ -1,26 +1,4 @@
-def interactive_menu
-    students = []
-    loop do
-        puts "1.Input the students"
-        puts "2. Show the students"
-        puts "9. Exit"
-        selection = gets.chomp
-        case selection
-        when "1"
-            students = input_students
-        when "2"
-            print_header
-            print(students)
-            print_footer(students)
-        when "9"
-            exit
-        else
-            puts "I don't know what you meant, try again"
-        end
-    end
-end
-
-
+@students = []
 
 def input_students
     puts "Please enter the names of the students"
@@ -37,7 +15,38 @@ def input_students
     students
 end
 
+def interactive_menu
+    lood do
+        process(gets.chomp)
+    end
+end
 
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_student_list
+    print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "9"
+        exit
+    else
+        puts "I don't know what you meant, try again"
+    end
+end
+
+    
 def print_header
     puts "The students of Vallains Academy"
     puts "_______________"
@@ -57,3 +66,5 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
+interactive_menu
